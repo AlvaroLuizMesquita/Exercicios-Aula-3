903669bbe207fde8550779e81ef3202001caaf59
AlvaroLuizMesquita <alvaro.mgnascimento@gmail.com>
2024-02-06 20:25:50 -0300

    Teste

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,16 +3,16 @@ console.log (`Olá ${nome}`);
 alert (`Olá ${nome}`);
 let linguagem = prompt ('Qual a linguagem de programação que você mais gosta?');
 console.log (`A linguagem que eu mais gosto? ${linguagem}`);
-let valor1 = 15
-let valor2 = 20
+let valor1 = 15;
+let valor2 = 20;
 let resultado = valor1 + valor2;
 console.log (`O resultado da soma entre o ${valor1} e o ${valor2} é = ${resultado}`);
-let valor3 = 55
-let valor4 = 40
+let valor3 = 55;
+let valor4 = 40;
 let resultado2 = valor3 - valor4
 console.log (`A diferença entre o ${valor3} e o ${valor4} é igual a ${resultado2}`);
 
-let idade = prompt ('Digite sua idade')
+let idade = prompt ('Digite sua idade');
     if (idade >= 18) {
         console.log ('Parabéns, já pode beber chopp');
     }
@@ -32,8 +32,8 @@ while (contador <= 10){
 }
 
 let nota = prompt ('Digite a Nota');
-    if (nota >= 7) {console.log (`Parabéns, sua nota final foi ${nota}. Você está APROVADO!`)}
-    else {console.log (`Infelizmente sua foi ${nota}. Logo, você foi REPROVADO!`)}
+    if (nota >= 7) {console.log (`Parabéns, sua nota final foi ${nota}. Você está APROVADO!`)};
+    else {console.log (`Infelizmente sua foi ${nota}. Logo, você foi REPROVADO!`)};
 
 let numeroAleatorio = Math.random ()
 console.log (numeroAleatorio);
